38855d0bba6e1c35d3ed7944e7b427092c45825b
Yura <yurapav99@gmail.com>
2019-01-13 19:27:09 +0200

    update

--- screens/LinksScreen.js
+++ screens/LinksScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { ScrollView, StyleSheet,View,Text } from 'react-native';
+import { ScrollView, StyleSheet,View,Text,Platform } from 'react-native';
 import {MyImage} from '../components/MyImage'
+import { MonoText } from '../components/StyledText';
 
 export default class LinksScreen extends React.Component {
   static navigationOptions = {
@@ -9,15 +10,26 @@ export default class LinksScreen extends React.Component {
 
   render() {
     return (
+      <View style={styles.container}>
+
+     <View style={styles.UserInfoContainer}>        
+         <Text style={styles. developmentModeText}>Yura Pavliuk</Text>
+          </View>
+      
+
+
       <ScrollView style={styles.container}>
         {/* Go ahead and delete ExpoLinksView and replace it with your
            * content, we just wanted to provide you with some helpful links */}
            <View style={styles.welcomeContainer}> 
-            <Text style={styles. developmentModeText}>Yura Pavliuk</Text>
+         
             <MyImage/>
+            <Text style={styles. developmentModeText}>Yura Pavliuk</Text>
              </View>
          
       </ScrollView>
+
+      </View>
     );
   }
 }
@@ -29,6 +41,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
   },
   welcomeContainer: {
+   
     alignItems: 'center',
     marginTop: 10,
     marginBottom: 20,
@@ -38,7 +51,17 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginTop: 10,
     marginBottom: 20,
-    fontSize: 14,
+    fontSize: 28,
+    backgroundColor: '#fbfbfb',
+    fontFamily: 'Cochin',
    
   },
+  UserInfoContainer: {
+position:"relative",
+ alignItems:"center",
+    top: 0,
+    left: 0,
+    opacity: 0.5,  
+    backgroundColor: 'rgba(96,100,109, 0.01)'
+    }
 });
